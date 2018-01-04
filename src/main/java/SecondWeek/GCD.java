fd78e1b7e330c0304081d460f6668e6e6e5dcb27
victor.bazan <victor.bazan@bearingpoint.com>
2018-01-04 11:40:19 +0000

    Created second week GCD class and test

---
+++ src/main/java/SecondWeek/GCD.java
@@ -0,0 +1,11 @@
+package SecondWeek;
+
+/**
+ * Created by victor on 04/01/18.
+    Greatest Common Divisor
+ */
+
+public class GCD {
+
+
+}
---
+++ src/test/java/SecondWeek/GCDTest.java
@@ -0,0 +1,10 @@
+package SecondWeek;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by victor on 04/01/18.
+ */
+public class GCDTest {
+
+}
