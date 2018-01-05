5d27a31bbb15b8709bcba726ea872a46beef85c1
victor.bazan <victor.bazan@bearingpoint.com>
2018-01-05 17:02:27 +0000

    GCD trivial and improved solutions and their tests

--- src/main/java/SecondWeek/GCD.java
+++ src/main/java/SecondWeek/GCD.java
@@ -6,6 +6,32 @@ package SecondWeek;
  */
 
 public class GCD {
+    private static int temp = 12;
+    public long naiveGCD (long a, long b) {
+       long divisor = 0l;
+
+        for (int i= 1; i<a+b; i++) {
+            if(a%i==0 && b%i==0) {
+                divisor=i;
+            }
+        }
+        return divisor;
+    }
+    //a > b
+    public long improvedGCD(long a , long b) {
+        if(b==0) {
+            return a;
+        }
+        else {
+            long aPrime = a % b;
+            return improvedGCD(b, aPrime);
+        }
+    }
+
+    public static void main (String args[]) {
+        GCD gcd = new GCD();
+        System.out.println(gcd.improvedGCD(3, 5));
 
 
+    }
 }
--- src/test/java/SecondWeek/GCDTest.java
+++ src/test/java/SecondWeek/GCDTest.java
@@ -1,10 +1,53 @@
 package SecondWeek;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by victor on 04/01/18.
  */
+@RunWith(JUnit4.class)
 public class GCDTest {
+
+    GCD gcd = new GCD();
+
+    @Test
+    public void testBasicGDC () {
+        assertEquals(5, gcd.naiveGCD(5, 10));
+        assertEquals(2, gcd.naiveGCD(4, 10));
+        assertEquals(234, gcd.naiveGCD(234, 234));
+        assertEquals(61232, gcd.naiveGCD(183696, 61232));
+    }
+
+    @Test
+    public void testImprovedGDC () {
+        assertEquals(5, gcd.improvedGCD(5, 10));
+        assertEquals(2, gcd.improvedGCD(4, 10));
+        assertEquals(234, gcd.improvedGCD(234, 234));
+        assertEquals(61232, gcd.improvedGCD(183696, 61232));
+    }
+
+    @Test
+    public void testTimingGDC () {
+
+        long startTime = System.currentTimeMillis();
+        assertEquals(61232, gcd.naiveGCD(3918848, 1653264));
+
+        long endTime   = System.currentTimeMillis();
+        long totalTimeClassic = endTime - startTime;
+
+
+        long startTimeImproved = System.currentTimeMillis();
+        assertEquals(61232, gcd.improvedGCD(3918848, 1653264));
+
+        long endTimeImproved   = System.currentTimeMillis();
+        long totalTimeImproved = endTimeImproved - startTimeImproved;
+        //Comparing processing times
+        System.out.println(totalTimeClassic + " > " + totalTimeImproved);
+        assertTrue(totalTimeClassic > totalTimeImproved);
+    }
 
 }
