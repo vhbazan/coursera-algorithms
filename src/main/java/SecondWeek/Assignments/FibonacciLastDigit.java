e4a9a1c56c6bf3f557cdd41b7a3280c84b16a235
victor.bazan <victor.bazan@bearingpoint.com>
2018-01-08 10:53:14 +0000

    Fibonacci Last Digit improvement

--- src/main/java/SecondWeek/Assignments/FibonacciLastDigit.java
+++ src/main/java/SecondWeek/Assignments/FibonacciLastDigit.java
@@ -3,7 +3,7 @@ package SecondWeek.Assignments;
 import java.util.*;
 
 public class FibonacciLastDigit {
-    private static int getFibonacciLastDigitNaive(int n) {
+    public int getFibonacciLastDigitNaive(int n) {
         if (n <= 1)
             return n;
 
@@ -19,10 +19,31 @@ public class FibonacciLastDigit {
         return current % 10;
     }
 
+    public int getFibonacciLastDigitImproved(int n) {
+        if (n <= 1)
+            return n;
+
+        int previous = 0;
+        int current  = 1;
+
+
+        for (int i = 0; i < n - 1; ++i) {
+            int tmp_previous = previous;
+            previous = current;
+            current = (tmp_previous + current)%10;
+            System.out.println(current + " ");
+
+        }
+
+        return current;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        FibonacciLastDigit fibonacciLastDigit = new FibonacciLastDigit();
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        int c = fibonacciLastDigit.getFibonacciLastDigitNaive(n);
         System.out.println(c);
     }
 }
+
